49d7a6e50d7b07311db98def474751b537df7d83
dani <dani@bigapps.co.il>
2022-02-09 17:29:26 +0200

    Update History Page

---
+++ lib/math_generator/models/point.dart
@@ -0,0 +1,10 @@
+
+
+class Point {
+  int? id;
+  int dateTimestemp;
+  int number;
+
+  Point({this.id,required this.dateTimestemp, required this.number});
+
+}
--- lib/pages/test_history_paga.dart
+++ lib/pages/test_history_paga.dart
@@ -1,21 +1,55 @@
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:math_world/localization/language_constants.dart';
 import 'package:math_world/math_generator/math_generator.dart';
+import 'package:math_world/math_generator/models/point.dart';
 import 'package:math_world/router/route_constants.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-
 
 class TestHistoryPage extends StatefulWidget {
+  Map<String, int> listMonths = {
+    "january": 1,
+    "february": 2,
+    "march": 3,
+    "april": 4,
+    "may": 5,
+    "june": 6,
+    "july": 7,
+    "august": 8,
+    "september": 9,
+    "october": 10,
+    "november": 11,
+    "december": 12,
+  };
+
   MathGenerator generator = new MathGenerator();
 
-  // List<Point> points = [
-  //
-  // ];
+  String currentMonth = "january";
+  List<Point> currentListPoints = [];
+
+  List<Point> points = [
+    Point(dateTimestemp: 1645392340000, number: 80),
+    Point(dateTimestemp: 9782964000000, number: 95),
+    Point(dateTimestemp: 1645392340000, number: 45),
+    Point(dateTimestemp: 1645392340000, number: 10),
+    Point(dateTimestemp: 1645392340000, number: 70),
+    Point(dateTimestemp: 1645392340000, number: 90),
+    Point(dateTimestemp: 1645392340000, number: 30),
+    Point(dateTimestemp: 1640392340000, number: 80),
+    Point(dateTimestemp: 1640392340000, number: 95),
+    Point(dateTimestemp: 1640392340000, number: 45),
+    Point(dateTimestemp: 1640392340000, number: 10),
+    Point(dateTimestemp: 1640392340000, number: 70),
+    Point(dateTimestemp: 1640392340000, number: 30),
+    Point(dateTimestemp: 1642392340000, number: 80),
+    Point(dateTimestemp: 1642392340000, number: 95),
+    Point(dateTimestemp: 1642392340000, number: 45),
+    Point(dateTimestemp: 1642392340000, number: 10),
+    Point(dateTimestemp: 1642392340000, number: 70),
+  ];
 
   final List<ChartTest> data = [
-
     ChartTest(
       number: 1,
       points: 80,
@@ -98,41 +132,124 @@ class TestHistoryPage extends StatefulWidget {
 }
 
 class _TestHistoryPageState extends State<TestHistoryPage> {
+  int indexList = 1;
+
+  @override
+  void initState() {
+    widget.currentMonth = widget.listMonths.keys.firstWhere(
+            (k) => widget.listMonths[k] == DateTime.now().month,
+        orElse: () => "january");
+    widget.currentListPoints = getListPoints();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-        appBar: AppBar(
-        backgroundColor: Color(0xff484443),),
+      appBar: AppBar(
+        backgroundColor: Color(0xff484443),
+      ),
       backgroundColor: Color(0xff256E59),
       body: Container(
-        child:
-              Center(
+        child: Center(
           // margin: EdgeInsets.all(20),
-          child:Column(
+          child: Column(
             children: [
-              SizedBox(height: 20,),
-
-              Expanded(child:new PointsChart( data: widget.data,)),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(10.0, 5, 10, 0),
+                  child: new Container(
+                    padding: const EdgeInsets.fromLTRB(10, 5, 5, 5),
+                    decoration: new BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: new BorderRadius.only(
+                            topLeft: const Radius.circular(5.0),
+                            bottomLeft: const Radius.circular(5.0),
+                            bottomRight: const Radius.circular(5.0),
+                            topRight: const Radius.circular(5.0))),
+                    child: new Center(
+                        child: new Column(children: [
+                      new DropdownButton<String>(
+                          underline: Text(''),
+                          icon: Icon(Icons.keyboard_arrow_down),
+                          hint: Center(child: new Text("0")),
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                          isExpanded: true,
+                          value: widget.currentMonth,
+                          onChanged: (newValue) {
+                            if (newValue != null)
+                              setState(() {
+                                widget.currentMonth = newValue;
+                                widget.currentListPoints = getListPoints();
+                              });
+                          },
+                          items: buildDropDownMenuItems(
+                              widget.listMonths.keys.toList())),
+                    ])),
+                  )),
+              Expanded(
+                  child: new PointsChart(
+                      data: widget.currentListPoints
+                          .map((point) => ChartTest(
+                              number: indexList++,
+                              points: point.number,
+                              barColor: point.number <= 50
+                                  ? charts.ColorUtil.fromDartColor(Colors.red)
+                                  : point.number < 80
+                                      ? charts.ColorUtil.fromDartColor(
+                                          Colors.yellow)
+                                      : charts.ColorUtil.fromDartColor(
+                                          Colors.green)))
+                          .toList())),
               FloatingActionButton.extended(
                 backgroundColor: Colors.deepOrange,
                 onPressed: () {
                   Navigator.pushNamed(context, registrationPage);
                 },
                 label: Text(
-                  getTranslated(context, 'registration')??"",
-                  style: GoogleFonts.courgette(color: Colors.white, fontSize: 18),
+                  getTranslated(context, 'registration') ?? "",
+                  style:
+                      GoogleFonts.courgette(color: Colors.white, fontSize: 18),
                 ),
               ),
               SizedBox(height: 20),
             ],
           ),
         ),
-
       ),
     );
   }
 
+  List<DropdownMenuItem<String>> buildDropDownMenuItems(
+      List<String> listItems) {
+    List<DropdownMenuItem<String>>? items = [];
+    for (String listItem in listItems) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(
+            listItem.toString(),
+            style: TextStyle(fontSize: 20),
+          ),
+          value: listItem.toString(),
+        ),
+      );
+    }
+    return items;
+  }
+
+  List<Point> getListPoints() {
+    var list = widget.points
+        .where((point) =>
+            DateTime.fromMicrosecondsSinceEpoch(point.dateTimestemp * 1000).month ==
+            widget.listMonths[widget.currentMonth])
+        .toList();
+    DateTime data =  DateTime.fromMicrosecondsSinceEpoch( DateTime.now().millisecondsSinceEpoch * 1000);
+    return list;
+  }
 }
 
 class ChartTest {
@@ -141,34 +258,32 @@ class ChartTest {
   final charts.Color barColor;
 
   ChartTest(
-      {
-         required this.number,
-         required this.points,
-         required this.barColor
-      }
-      );
+      {required this.number, required this.points, required this.barColor});
 }
 
-
-class PointsChart extends StatelessWidget {
+class PointsChart extends StatefulWidget {
   final List<ChartTest> data;
 
   PointsChart({required this.data});
+
+  @override
+  _PointsChartState createState() => _PointsChartState();
+}
+
+class _PointsChartState extends State<PointsChart> {
+
   @override
   Widget build(BuildContext context) {
-
     List<charts.Series<ChartTest, String>> series = [
       charts.Series(
           id: "points",
-          data: data,
+          data: widget.data,
           domainFn: (ChartTest series, _) => series.number.toString(),
           measureFn: (ChartTest series, _) => series.points,
-          colorFn: (ChartTest series, _) => series.barColor
-      )
+          colorFn: (ChartTest series, _) => series.barColor)
     ];
 
     return Expanded(
-
       child: Card(
         child: Padding(
           padding: const EdgeInsets.all(9.0),
@@ -187,5 +302,6 @@ class PointsChart extends StatelessWidget {
       ),
     );
   }
+
 
 }
